25b75e214792b6b5bc0a50e10d2e57f287e0a544
Federico Barreiro <fed_eh@hotmail.com>
2024-07-21 23:18:43 -0300

    first commit - add header, footer, models

---
+++ frontend/Header.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
+import SearchBox from './SearchBox';
+//import logo from '../assets/g.jpg';
+import { resetCart } from '../slices/cartSlice';
+
+const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      dispatch(resetCart());
+      navigate('/login');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <header className='beige2'>
+      <Navbar bg='beige' variant='light' expand='lg' collapseOnSelect className='crema'>
+        <Container>
+          <Navbar.Brand as={Link} to='/' > {/* Color de texto blanco */}
+            
+            Goodread
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <SearchBox  />
+              <Nav.Link as={Link} to='/cart' > {/* Color de texto blanco */}
+                <FaShoppingCart /> Cart
+                {cartItems.length > 0 && (
+                  <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                  </Badge>
+                )}
+              </Nav.Link >
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='username' style={{ color: 'white' }}> {/* Color de texto blanco */}
+                    <NavDropdown.Item as={Link} to='/profile'>
+                      Profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <Nav.Link as={Link} to='/login' > {/* Color de texto blanco */}
+                  <FaUser /> Sign In
+                </Nav.Link>
+              )}
+              {/* Admin Links */}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu' style={{ color: 'white' }}> {/* Color de texto blanco */}
+                  <NavDropdown.Item as={Link} to='/admin/booklist'>
+                    Books
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='/admin/orderlist'>
+                    Orders
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='/admin/userlist'>
+                    Users
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
